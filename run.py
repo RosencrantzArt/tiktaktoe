c9fcd721ccc2e97272f49005cd9e4bde1b1c8e0b
Anna Rosencrantz <anrosencrantz79@icloud.com>
2024-05-29 09:09:43 +0000

    create function for board

--- run.py
+++ run.py
@@ -5,3 +5,16 @@ from colorama import Fore, Style
 
 coloroma.init()
 
+
+def print_board(board):
+    """
+    Prints the current state of the game board. 
+
+    Args:
+    board (list of list of str): The game board to print.
+    """
+    os.system('cls' if os.name == 'nt' else 'clear')
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 5)
+    
